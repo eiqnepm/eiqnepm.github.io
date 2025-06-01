8d198b9f2b29c08cab7a0f4b50c2ae42947364dc
eiqnepm <86803173+eiqnepm@users.noreply.github.com>
2025-06-01 22:37:11 +0100

    Update page.tsx

--- app/names/page.tsx
+++ app/names/page.tsx
@@ -1,3 +1,30 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Names() {
-  return <p>Names</p>;
+  const [names, setNames] = useState<string[]>([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        "https://www.reddit.com/api/v1/generate_username.json"
+      );
+      const data = await response.json();
+      setNames(data.usernames);
+      setLoading(false);
+    })();
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (!names) return <p>No names</p>;
+
+  return (
+    <ul>
+      {names.map((username: any) => (
+        <li key={username}>{username}</li>
+      ))}
+    </ul>
+  );
 }
